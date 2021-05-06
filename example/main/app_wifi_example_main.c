ce197b1db3099a3b98201bdeefc21440255c96da
Michal Dvorak <mikee2185@gmail.com>
2021-05-06 17:47:13 +0200

    use qrcode print from rainmaker as an example
    any QR code online service can be used

--- example/main/app_wifi_example_main.c
+++ example/main/app_wifi_example_main.c
@@ -1,15 +1,19 @@
 #include <app_wifi.h>
 #include <esp_log.h>
 #include <nvs_flash.h>
-#include <wifi_provisioning/manager.h>
 
 static const char TAG[] = "example";
 
-static void print_pop_handler(__unused void *arg, __unused esp_event_base_t event_base,
-                              __unused int32_t event_id, __unused void *event_data)
+static void print_qrcode_handler(__unused void *arg, __unused esp_event_base_t event_base,
+                                 __unused int32_t event_id, __unused void *event_data)
 {
-    ESP_LOGI(TAG, "Service name: %s", app_wifi_prov_get_service_name());
-    ESP_LOGI(TAG, "Proof of possession: %s", app_wifi_get_prov_pop());
+    const char VER[] = "v1";
+    char payload[200] = {};
+    // {"ver":"%s","name":"%s","pop":"%s","transport":"%s"}
+    snprintf(payload, sizeof(payload), "%%7B%%22ver%%22%%3A%%22%s%%22%%2C%%22name%%22%%3A%%22%s%%22%%2C%%22pop%%22%%3A%%22%s%%22%%2C%%22transport%%22%%3A%%22%s%%22%%7D",
+             VER, app_wifi_prov_get_service_name(), app_wifi_get_prov_pop(), APP_WIFI_PROV_TRANSPORT);
+    // NOTE print this regardless of log level settings
+    printf("PROVISIONING: To view QR Code, copy paste the URL in a browser:\n%s?data=%s\n", "https://rainmaker.espressif.com/qrcode.html", payload);
 }
 
 void app_main()
@@ -27,7 +31,7 @@ void app_main()
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     // Print PoP on provisioning
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_PROV_EVENT, WIFI_PROV_START, &print_pop_handler, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_PROV_EVENT, WIFI_PROV_START, &print_qrcode_handler, NULL, NULL));
 
     // Setup WiFi
     struct app_wifi_config wifi_cfg = {
