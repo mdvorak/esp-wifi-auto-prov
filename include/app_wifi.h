403f3eb2f0149fa9b387fbb6106fc5561eeb45a0
Michal Dvorak <mikee2185@gmail.com>
2022-01-08 11:41:18 +0100

    fix: added old compatibility headers
    backward compatible headers with pragma message to emit compiler warning

---
+++ include/app_wifi.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// DEPRECATION NOTICE
+#pragma message("header app_wifi.h is deprecated, please use wifi_prov.h instead")
+
+#include "wifi_prov.h"
---
+++ include/app_wifi_defs.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// DEPRECATION NOTICE
+#pragma message("header app_wifi_defs.h is deprecated, please use wifi_prov_defs.h instead")
+
+#include "wifi_prov_defs.h"
